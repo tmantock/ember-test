a0aee0c4f8913a060de414bcab7fb7a38e7992cc
Tevin Mantock <tmantock@gmail.com>
2018-05-12 06:43:31 -0700

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,7 @@ module.exports = function (environment) {
 
     ENV['jux'] = {
       APP_ID: 'ember-test',
-      BRANCH_ID: 'xj9',
+      BRANCH_ID: 'dev',
       CAPTURE_INSTANCE_URL: 'http://localhost:3001',
     }
   }
