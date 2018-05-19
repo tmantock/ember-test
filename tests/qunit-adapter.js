07809d87b61a168c348a315ad91218b0128e4788
Tevin Mantock <tmantock@gmail.com>
2018-05-19 14:45:56 -0700

    Create qunit-adapter.js

---
+++ tests/qunit-adapter.js
@@ -0,0 +1,13 @@
+import { QUnitAdapter } from 'ember-qunit';
+
+let useEmberOnError = false;
+export default QUnitAdapter.extend({
+  exception(error) {
+    if (useEmberOnError) {
+      Ember.onerror(error);
+      return;
+    }
+
+    this._super(...arguments);
+  }
+})
