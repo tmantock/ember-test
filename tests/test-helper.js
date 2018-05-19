c67d6cefd29e08777da7c89d5b8ee96000ac88ca
Tevin Mantock <tmantock@gmail.com>
2018-05-19 14:47:28 -0700

    Update test-helper.js

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,7 +2,11 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import QUnitAdapter from './qunit-adapter';
+import Ember from 'ember';
 
 setApplication(Application.create(config.APP));
 
 start({setupTestAdapter: false});
+
+Ember.Test.adapter = QUnitAdapter.create();
